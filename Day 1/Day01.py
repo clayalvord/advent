44dd1a7c7635c25e1e8e4e97548402eb8702aab0
clayalvord <clayalvord@gmail.com>
2023-12-03 14:28:58 -0800

    figuring it out

---
+++ Day 1/Day01.py
@@ -0,0 +1,12 @@
+input = "./input.txt"
+
+try:
+    with open(input, 'r') as file:
+        # Read and print the contents of the file
+        file_contents = file.read()
+        print("File contents:")
+        print(file_contents)
+except FileNotFoundError:
+    print(f"Error: File '{input}' not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
