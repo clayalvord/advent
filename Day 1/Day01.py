e00d36f92258f2e4aecf258f229efc61a1d90d83
clayalvord <clayalvord@gmail.com>
2023-12-03 13:05:48 -0800

    first

--- Day 1/Day01.py
+++ Day 1/Day01.py
@@ -1,4 +1,14 @@
-def read_print(input):
-    lines = input.splitlines()
-    print(lines)
-    return None
+# Open the file in the same directory as the script
+file_name = "input.txt"
+file_path = "./" + file_name  # Assuming the file is in the same directory as the script
+
+try:
+    with open(file_path, 'r') as file:
+        # Read and print the contents of the file
+        file_contents = file.read()
+        print("File contents:")
+        print(file_contents)
+except FileNotFoundError:
+    print(f"Error: File '{file_name}' not found.")
+except Exception as e:
+    print(f"An error occurred: {e}")
