e7bcf019e6978749e9ee101c93844de2b9ffec41
clayalvord <clayalvord@gmail.com>
2023-12-03 20:45:57 -0800

    updated to only show the result not every row

--- Day 1/Day01_Part02.py
+++ Day 1/Day01_Part02.py
@@ -1,23 +1,28 @@
 input_file = "Day 1/input.txt"
 
-# Initialize variable to store the total sum
+# variable to store the total sum
 total_sum = 0
 
 try:
     with open(input_file, 'r') as text_file:
+        
         # Iterate through each line in the file
         for line in text_file:
-            # Initialize variables to store the first and last numbers for each row
+            
+            # variables to store the first and last numbers for each row
             first_number = None
             last_number = None
 
             # Iterate through each character in the line
             for char in line:
+
                 # Check if the character is a digit
                 if char.isdigit():
+
                     # If it's the first number, store it
                     if first_number is None:
                         first_number = char
+                    
                     # Always update the last number
                     last_number = char
 
@@ -27,11 +32,6 @@ try:
             # Sum the results for each row
             if two_digit_number is not None:
                 total_sum += two_digit_number
-
-                print(f"Row: {line.strip()}, Concatenated 2-Digit Number: {two_digit_number:02d}")
-
-            else:
-                print(f"Row: {line.strip()}, No numbers found in the row.")
 
     # Print the total sum
     print(f"Total Sum of Concatenated 2-Digit Numbers: {total_sum:02d}")
