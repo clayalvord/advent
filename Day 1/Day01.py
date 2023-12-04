ca81d2107e8132a1aeea9c9a68490f3902cc6404
clayalvord <clayalvord@gmail.com>
2023-12-03 20:05:36 -0800

    updated to only look at the first row in the input file

--- Day 1/Day01.py
+++ Day 1/Day01.py
@@ -1,16 +1,16 @@
-input = "Day 1/input.txt"
+input_file = "Day 1/input.txt"
 
 # Initialize variables to store the first and last numbers
 first_number = None
 last_number = None
 
 try:
-    with open(input, 'r') as text_file:
-        # Read the entire content of the file
-        content = text_file.read()
+    with open(input_file, 'r') as text_file:
+        # Read only the first line of the file
+        first_line = text_file.readline()
 
-        # Iterate through each character in the content
-        for char in content:
+        # Iterate through each character in the first line
+        for char in first_line:
             # Check if the character is a digit
             if char.isdigit():
                 # If it's the first number, store it
@@ -19,18 +19,16 @@ try:
                 # Always update the last number
                 last_number = char
 
-    # Print the results
-    if first_number is not None:
-        print(f"First Number: {first_number}")
-    else:
-        print("No numbers found in the file.")
+    # Concatenate the first and last numbers to make a 2-digit number
+    two_digit_number = int(str(first_number) + str(last_number)) if first_number is not None and last_number is not None else None
 
-    if last_number is not None:
-        print(f"Last Number: {last_number}")
+    # Print the results
+    if two_digit_number is not None:
+        print(f"Concatenated 2-Digit Number: {two_digit_number:02d}")
     else:
         print("No numbers found in the file.")
 
 except FileNotFoundError:
-    print(f"Error: File '{input}' not found.")
+    print(f"Error: File '{input_file}' not found.")
 except Exception as e:
     print(f"An error occurred: {e}")
