f94f52cafc8cc609011f097789211b50a5b30feb
clayalvord <clayalvord@gmail.com>
2023-12-10 20:05:01 -0800

    suming the first row by color

--- Day 2/Day02.py
+++ Day 2/Day02.py
@@ -4,21 +4,30 @@ input_file = "Day 2/input.txt"
 
 try:
     with open(input_file, 'r') as text_file:
-        # Iterate through each line in the file
-        for line in text_file:
-            # Split each line at the colon (:) and take the right part
-            line_data = line.split(':', 1)[-1]
-            
-            # Extract all numbers from the right part using regular expression
-            numbers = [int(match) for match in re.findall(r'\d+', line_data)]
-            
-            # Calculate the sum of numbers on the current line
-            line_sum = sum(numbers)
-            
-            # Print the result
-            print(f"Line: {line.strip()}, Sum: {line_sum}")
+        # Dictionary to store sums per color
+        color_sums = {'red': 0, 'green': 0, 'blue': 0}
+
+        # Process only the first line in the file
+        line = next(text_file)
+
+        # Split the line at the colon (:) and take the right part
+        line_data = line.split(':', 1)[-1]
+
+        # Extract all numbers and colors from the right part using regular expressions
+        matches = re.findall(r'(\d+)\s*([a-zA-Z]+)', line_data)
+
+        # Process each match
+        for number, color in matches:
+            # Sum the numbers per color
+            color_sums[color.lower()] += int(number)
+
+        # Print the results for the first row
+        for color, total_sum in color_sums.items():
+            print(f"Total sum for {color.capitalize()} in the first row: {total_sum}")
 
 except FileNotFoundError:
     print(f"Error: File '{input_file}' not found.")
+except StopIteration:
+    print("File is empty.")
 except Exception as e:
     print(f"An error occurred: {e}")
