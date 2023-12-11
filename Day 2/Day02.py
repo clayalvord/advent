12c20c47d725996ff0a682d9e5c181c82c87ed07
clayalvord <clayalvord@gmail.com>
2023-12-10 20:08:23 -0800

    sum per split and scratch pad update

--- Day 2/Day02.py
+++ Day 2/Day02.py
@@ -4,26 +4,30 @@ input_file = "Day 2/input.txt"
 
 try:
     with open(input_file, 'r') as text_file:
-        # Dictionary to store sums per color
-        color_sums = {'red': 0, 'green': 0, 'blue': 0}
-
         # Process only the first line in the file
         line = next(text_file)
 
-        # Split the line at the colon (:) and take the right part
-        line_data = line.split(':', 1)[-1]
+        # Split the line at semicolons (;)
+        splits = line.split(';')
 
-        # Extract all numbers and colors from the right part using regular expressions
-        matches = re.findall(r'(\d+)\s*([a-zA-Z]+)', line_data)
+        # Iterate through each split in the first row
+        for split in splits:
+            # Dictionary to store sums per color for each split
+            color_sums = {'red': 0, 'green': 0, 'blue': 0}
 
-        # Process each match
-        for number, color in matches:
-            # Sum the numbers per color
-            color_sums[color.lower()] += int(number)
+            # Extract all numbers and colors from the split using regular expressions
+            matches = re.findall(r'(\d+)\s*([a-zA-Z]+)', split)
 
-        # Print the results for the first row
-        for color, total_sum in color_sums.items():
-            print(f"Total sum for {color.capitalize()} in the first row: {total_sum}")
+            # Process each match
+            for number, color in matches:
+                # Sum the numbers per color
+                color_sums[color.lower()] += int(number)
+
+            # Print the results for each split
+            print(f"Results for split: {split.strip()}")
+            for color, total_sum in color_sums.items():
+                print(f"Total sum for {color.capitalize()}: {total_sum}")
+            print("-" * 30)  # Separate results for each split
 
 except FileNotFoundError:
     print(f"Error: File '{input_file}' not found.")
