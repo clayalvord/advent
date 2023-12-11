a8ebcfad011ffd6dc2b9312579ce2cd6dc05d9e2
clayalvord <clayalvord@gmail.com>
2023-12-10 19:46:54 -0800

    sums all numbers in each row

--- Day 2/Day02.py
+++ Day 2/Day02.py
@@ -1,11 +1,19 @@
+import re
+
 input_file = "Day 2/input.txt"
 
 try:
     with open(input_file, 'r') as text_file:
         # Iterate through each line in the file
         for line in text_file:
-            # Process each line as needed
-            print(line.strip())  # Example: print each line without leading/trailing whitespaces
+            # Extract all numbers from the line using regular expression
+            numbers = [int(match) for match in re.findall(r'\d+', line)]
+            
+            # Calculate the sum of numbers on the current line
+            line_sum = sum(numbers)
+            
+            # Print the result
+            print(f"Line: {line.strip()}, Sum: {line_sum}")
 
 except FileNotFoundError:
     print(f"Error: File '{input_file}' not found.")
