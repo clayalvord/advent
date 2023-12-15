784d737ee384b18f30277a230025c37bb91f9c0e
clayalvord <clayalvord@gmail.com>
2023-12-14 20:45:15 -0800

    identify symbols

--- Day 3/Day03.py
+++ Day 3/Day03.py
@@ -4,11 +4,18 @@ file_path = "Day 3/input.txt"
 try:
     with open(file_path, 'r') as file:
         # Read the content of the file
-        file_content = file.read()
+        file_content = file.readlines()
 
-        # Display or process the content as needed
-        print("File Content:")
-        print(file_content)
+        # Process each row
+        for line_number, row in enumerate(file_content, start=1):
+            symbols = {'*': 0, '#': 0, '+': 0, '$': 0}
+
+            # Count the occurrences of each symbol in the row
+            for symbol in symbols:
+                symbols[symbol] = row.count(symbol)
+
+            # Display or process the count of each symbol in the row
+            print(f"Row {line_number}: {symbols}")
 
 except FileNotFoundError:
     print(f"Error: File '{file_path}' not found.")
