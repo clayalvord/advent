7f8ce635a5088b7813b851b08e0781d3d2a5024e
clayalvord <clayalvord@gmail.com>
2023-12-16 19:40:50 -0800

    finds numbers to the left or right of symbols

--- Day 3/Day03.py
+++ Day 3/Day03.py
@@ -6,11 +6,13 @@ file_path = os.path.join("Day 3", "input.txt")
 try:
     with open(file_path, 'r') as file:
         file_content = file.readlines()
-        part_number_pattern = re.compile(r'\b\d+\b')
+
+        # Updated part_number_pattern to capture numbers immediately following a symbol
+        part_number_pattern = re.compile(r'[*#+$](\d+)|(\d+)[*#+$]')
 
         for line_number, row in enumerate(file_content, start=1):
             symbols = re.findall(r'[*#+$]', row)
-            part_numbers = part_number_pattern.findall(row)
+            part_numbers = [match.group(1) or match.group(2) for match in part_number_pattern.finditer(row)]
 
             print(f"Row {line_number}: Symbols: {symbols}, Part Numbers: {part_numbers}")
 
