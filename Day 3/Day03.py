1c29531482b7a6e3414fa1658963197da57f9b01
clayalvord <clayalvord@gmail.com>
2023-12-16 21:15:36 -0800

    foo

--- Day 3/Day03.py
+++ Day 3/Day03.py
@@ -1,17 +1,16 @@
-import os
 import re
 
-file_path = os.path.join("Day 3", "input.txt")
+# Function to extract numbers from a given string using regular expression
+def extract_numbers(input_string):
+    return re.findall(r'\d+', input_string)
 
-try:
-    with open(file_path, 'r') as file:
-        for line_number, row in enumerate(file, start=1):
-            part_numbers = [match for group in re.findall(r'[*#+$](\d+)|(\d+)[*#+$]', row) for match in group if match]
-            print(f"Row {line_number}: {', '.join(part_numbers)}")
+# Read input file and process each line
+input_file_path = "Day 3/input.txt"
 
-except FileNotFoundError as e:
-    print(f"Error: {e}")
-except IOError as e:
-    print(f"IOError: {e}")
-except Exception as e:
-    print(f"An error occurred: {e}")
+with open(input_file_path, 'r') as file:
+    for line_number, line in enumerate(file, start=1):
+        # Extract numbers from the current line
+        part_numbers = extract_numbers(line)
+        
+        # Print the result
+        print(f"Row {line_number}: {', '.join(part_numbers)}")
