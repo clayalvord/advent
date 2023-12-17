d15802c62d3bba1ecac42983380e8a8e86d26cbe
clayalvord <clayalvord@gmail.com>
2023-12-16 19:29:21 -0800

    simpler output

--- Day 3/Day03.py
+++ Day 3/Day03.py
@@ -1,30 +1,22 @@
+import os
 import re
 
-# Assuming the input file is in the "Day 3" directory
-file_path = "Day 3/input.txt"
+file_path = os.path.join("Day 3", "input.txt")
 
 try:
     with open(file_path, 'r') as file:
-        # Read the content of the file
         file_content = file.readlines()
+        part_number_pattern = re.compile(r'\b\d+\b')
 
-        # Define the regular expression pattern to identify part numbers
-        part_number_pattern = r'\b\d+\b'
-
-        # Process each row
         for line_number, row in enumerate(file_content, start=1):
-            # Find all symbols in the row
-            symbols = {'*': 0, '#': 0, '+': 0, '$': 0}
-            for symbol in symbols:
-                symbols[symbol] = row.count(symbol)
+            symbols = re.findall(r'[*#+$]', row)
+            part_numbers = part_number_pattern.findall(row)
 
-            # Find all part numbers in the row using the regular expression
-            part_numbers = re.findall(part_number_pattern, row)
-
-            # Display or process the symbols and part numbers found in the row
             print(f"Row {line_number}: Symbols: {symbols}, Part Numbers: {part_numbers}")
 
 except FileNotFoundError:
     print(f"Error: File '{file_path}' not found.")
+except IOError as e:
+    print(f"IOError: {e}")
 except Exception as e:
     print(f"An error occurred: {e}")
