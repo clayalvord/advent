da2cef7ad296d23dcb578b035b35b3aa52b89f38
clayalvord <clayalvord@gmail.com>
2023-12-21 20:20:57 -0800

    read file

--- Day 4/Day04.py
+++ Day 4/Day04.py
@@ -1,33 +1,20 @@
-def calculate_points(card):
-    # Split the card into winning numbers and numbers you have
-    winning_numbers, your_numbers = map(str.split, card.split('|'))
+# Filename: read_input.py
 
-    # Convert the lists to sets for easy comparison
-    winning_numbers = set(map(int, winning_numbers))
-    your_numbers = set(map(int, your_numbers))
-
-    # Find the common numbers and calculate points
-    common_numbers = your_numbers.intersection(winning_numbers)
-    points = sum([2 ** i for i in range(len(common_numbers))])
-
-    return points
-
-def main():
-    # Read input from file
-    file_path = "Day 4/input.txt"
-    with open(file_path, "r") as file:
-        puzzle_input = file.read().splitlines()
-
-    total_points = 0
-    for card in puzzle_input:
-        # Skip lines that don't contain the expected format
-        if "|" not in card:
-            continue
-
-        points = calculate_points(card)
-        total_points += points
-
-    print("Total points:", total_points)
+def read_input_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            return content
+    except FileNotFoundError:
+        print(f"Error: File not found at {file_path}")
+        return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
 if __name__ == "__main__":
-    main()
+    input_file_path = "Day 4/input.txt"  # Replace with the correct path to your input file
+    file_content = read_input_file(input_file_path)
+
+    if file_content is not None:
+        print(f"Contents of the file:\n{file_content}")
