10fdb4fc784fae10696c9e151b69ffc5160c7c6c
clayalvord <clayalvord@gmail.com>
2023-12-21 19:06:03 -0800

    reads file into memory

--- Day 3/Day03.py
+++ Day 3/Day03.py
@@ -11,49 +11,49 @@ def find_symbols_positions(line, symbols):
             positions[symbol] = symbol_positions
     return positions
 
-def scan(line, symbol_position):
+def scan(lines, row_num, symbol_position):
     approved_numbers = []
     
     # Look 1 position to the right
-    match_right = re.search(r'\d+', line[symbol_position:])
+    match_right = re.search(r'\d+', lines[row_num][symbol_position:])
     if match_right:
         approved_numbers.append(int(match_right.group()))
 
     # Look 1 position to the left
-    match_left = re.search(r'\d+', line[:symbol_position])
+    match_left = re.search(r'\d+', lines[row_num][:symbol_position])
     if match_left:
         approved_numbers.append(int(match_left.group()))
 
     # Look at symbol_position on the preceding row
-    match_current = re.search(r'\d+', line)
+    match_current = re.search(r'\d+', lines[row_num])
     if match_current:
         approved_numbers.append(int(match_current.group()))
 
     # Look at symbol_position on the following row
-    if symbol_position < len(line):
-        match_below = re.search(r'\d+', line)
+    if row_num + 1 < len(lines):
+        match_below = re.search(r'\d+', lines[row_num + 1])
         if match_below:
             approved_numbers.append(int(match_below.group()))
 
     # Look at symbol_position + 1 on the preceding row
-    match_above_right = re.search(r'\d+', line)
+    match_above_right = re.search(r'\d+', lines[row_num])
     if match_above_right:
         approved_numbers.append(int(match_above_right.group()))
 
     # Look at symbol_position - 1 on the preceding row
-    match_above_left = re.search(r'\d+', line)
+    match_above_left = re.search(r'\d+', lines[row_num])
     if match_above_left:
         approved_numbers.append(int(match_above_left.group()))
 
     # Look at symbol_position + 1 on the following row
-    if symbol_position < len(line):
-        match_below_right = re.search(r'\d+', line)
+    if row_num + 1 < len(lines):
+        match_below_right = re.search(r'\d+', lines[row_num + 1])
         if match_below_right:
             approved_numbers.append(int(match_below_right.group()))
 
     # Look at symbol_position - 1 on the following row
-    if symbol_position < len(line):
-        match_below_left = re.search(r'\d+', line)
+    if row_num + 1 < len(lines):
+        match_below_left = re.search(r'\d+', lines[row_num + 1])
         if match_below_left:
             approved_numbers.append(int(match_below_left.group()))
 
@@ -63,15 +63,25 @@ def process_input_file(file_path):
     symbols_to_find = "*#+$"
     
     with open(file_path, 'r') as file:
-        for row_num, line in enumerate(file, start=1):
+        lines = file.readlines()
+
+    row_num = 0
+    for line in lines:
+        line = line.strip()
+        if line:
+            row_num += 1
             part_numbers = extract_numbers_from_line(line)
             symbol_positions = find_symbols_positions(line, symbols_to_find)
+            
+            row_data = f"Row {row_num} [{', '.join(map(str, part_numbers))}]"
             
             if symbol_positions:
                 for symbol, positions in symbol_positions.items():
                     for symbol_position in positions:
-                        approved_numbers = scan(line, symbol_position)
-                        print(f"Row {row_num}, Approved Numbers for {symbol} at position {symbol_position}: {approved_numbers}")
+                        approved_numbers = scan(lines, row_num - 1, symbol_position - 1)
+                        print(f"{row_data} for {symbol} at position {symbol_position} [{', '.join(map(str, approved_numbers))}]")
+            else:
+                print(row_data)
 
 if __name__ == "__main__":
     input_file_path = "Day 3/input.txt"
